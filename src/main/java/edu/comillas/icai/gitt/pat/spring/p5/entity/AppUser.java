9e35f78ff7576fe5e9c68064d8f6fa288555b2e4
MiguelHerremansHumada <2021128238@alu.comillas.edu>
2025-04-19 12:41:01 +0200

    Commit #TODO2

--- src/main/java/edu/comillas/icai/gitt/pat/spring/p5/entity/AppUser.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/p5/entity/AppUser.java
@@ -18,6 +18,64 @@ import jakarta.persistence.Id;
  * - name
  */
 
+@Entity(name = "APP_USER")
 public class AppUser {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Role role;
+
+    // Getters y setters (o usa Lombok si está permitido)
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
